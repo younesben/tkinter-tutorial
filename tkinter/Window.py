903e4e44188bb06ee78637b0f0dfe44b0a3a66ba
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-05 11:04:33 -0600

    Update Window.py

--- tkinter/Window.py
+++ tkinter/Window.py
@@ -1,6 +1,7 @@
-from Tkinter import *
+from Tkinter import Tk, BOTH
+from ttk import Style, Frame, Button
 
-# Inherits the Frame class. "Self" is an instance of Frame
+# Inherits the Frame class from ttk. "Self" is an instance of Frame
 class Window(Frame):
 
 	# Constructor
@@ -9,7 +10,7 @@ class Window(Frame):
 		Calling the constructor method of the inherited class. background specifies
 		the color of the Frame widget
 		'''
-		Frame.__init__(self, parent, background = "white")
+		Frame.__init__(self, parent)
 
 		# Instance variables of the class
 		self.parent = parent
@@ -17,19 +18,28 @@ class Window(Frame):
 		# Call the method to create the window 
 		self.initUI()
 
+	# Initialize the UI
 	def initUI(self):
+		# Set the style
+		self.style()
+
 		# Set the tiltle of the window
-		self.parent.title("Simple")
+		self.parent.title("First example")
 
 		'''
 		Geometry manager. Organizes widgets into horizontal and vertical boxes. The 
 		Frame widget is place on the root window expanding on both sides
 		'''
 		self.pack(fill = BOTH, expand = 1)
+
+		# Add QUIT button
+		quit_button = Button(self, text = "Quit", command = self.quit)
+		quit_button.place(x = 50, y = 50)
 		
 		# Call method tho center window
 		self.centerWindow()
 
+	# Center the window
 	def centerWindow(self):
 		width = 290
 		height = 150
@@ -41,3 +51,8 @@ class Window(Frame):
 		y = (screen_height - height)/2
 
 		self.parent.geometry('%dx%d+%d+%d' % (width, height, x, y))
+
+		# Adding a them to all widgets
+	def style(self):
+		self.style = Style()
+		self.style.theme_use("default")
