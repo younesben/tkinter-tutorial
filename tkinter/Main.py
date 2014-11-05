5a29931f415b352a2c6f9fe22abbbeac673b1483
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-05 11:04:51 -0600

    Update Main.py

--- tkinter/Main.py
+++ tkinter/Main.py
@@ -2,7 +2,7 @@ from Tkinter import *
 from Window import *
 
 def Main():
-	# Main window is created
+	# Main window is created. Must be created before any widgets
 	root = Tk()
 
 	# Setting a size and position for the window on the screen
@@ -10,6 +10,11 @@ def Main():
 
 	# Creating an instance of the class window
 	main_window = Window(root)
+
+	'''
+	Entering the mainloop to handle events from the window system.
+	These are dispatched to application widgets
+	'''
 	main_window.mainloop()
 
 if __name__ == '__main__':
