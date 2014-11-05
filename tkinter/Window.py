38f3d6f27fe88d36c2f2d2d7e55d54b3b8b3b977
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-05 10:23:40 -0600

    Update and rename window.py to Window.py

--- tkinter/Window.py
+++ tkinter/Window.py
@@ -1,16 +1,33 @@
 from Tkinter import *
 
-class window(Frame):
+# Inherits the Frame class. "Self" is an instance of Frame
+class Window(Frame):
 
+	# Constructor
 	def __init__(self, parent):
+		'''
+		Calling the constructor method of the inherited class. background specifies
+		the color of the Frame widget
+		'''
 		Frame.__init__(self, parent, background = "white")
 
+		# Instance variables of the class
 		self.parent = parent
+
+		# Call the method to create the window 
 		self.initUI()
 
 	def initUI(self):
+		# Set the tiltle of the window
 		self.parent.title("Simple")
+
+		'''
+		Geometry manager. Organizes widgets into horizontal and vertical boxes. The 
+		Frame widget is place on the root window expanding on both sides
+		'''
 		self.pack(fill = BOTH, expand = 1)
+		
+		# Call method tho center window
 		self.centerWindow()
 
 	def centerWindow(self):
