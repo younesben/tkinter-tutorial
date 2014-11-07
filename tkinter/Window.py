85767af01a8c354cf63855900bdad057348c7556
Miguel Velez <miguelvelez@mijecu25.com>
2014-11-06 19:45:44 -0600

    11/6/14 This is an scripting language

--- tkinter/Window.py
+++ tkinter/Window.py
@@ -56,3 +56,22 @@ class Window(Frame):
 	def style(self):
 		self.style = Style()
 		self.style.theme_use("default")
+
+def Main():
+	# Main window is created. Must be created before any widgets
+	root = Tk()
+
+	# Setting a size and position for the window on the screen
+	root.geometry("250x150+300+300")
+
+	# Creating an instance of the class window
+	main_window = Window(root)
+
+	'''
+	Entering the mainloop to handle events from the window system.
+	These are dispatched to application widgets
+	'''
+	main_window.mainloop()
+
+if __name__ == '__main__':
+	Main()
